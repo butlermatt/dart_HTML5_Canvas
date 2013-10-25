83c3cabe109bbefb2fa79b6da40b9a16cc30a41e
Matthew Butler <butler.matthew@gmail.com>
2013-10-25 16:39:56 -0300

    Added arcTo and Quadradic and Bezier curves
    Color coded each type of sample

--- web/ch2/ch2ex4.dart
+++ web/ch2/ch2ex4.dart
@@ -23,14 +23,48 @@ class CanvasApp {
   void drawScreen() {
     // Content goes here.
     
+    // Arc examples
     context..strokeStyle = 'black'
         ..lineWidth = 5
         ..beginPath()
         // Full circle. In Dart anticlockwise is optional (defaults to false)
-        ..arc(100, 100, 20, (math.PI/180) * 0, (math.PI/180) * 360)
+        ..arc(50, 100, 20, (math.PI/180) * 0, (math.PI/180) * 360)
+        ..stroke()
+        ..closePath()
+        // A quarter circle arc
+        ..beginPath()
+        ..arc(50, 200, 20, (math.PI/180) * 0, (math.PI/180) * 90)
+        ..stroke()
+        ..closePath()
+        // A Three-quarter circle by using anticlockwise
+        ..beginPath()
+        ..arc(50, 300, 20, (math.PI/180) * 0, (math.PI/180) * 90, true)
         ..stroke()
         ..closePath();
     
+    // arcTo Examples
+    context..strokeStyle = 'red'
+        ..beginPath()
+        ..moveTo(100, 0)
+        ..lineTo(200, 200)
+        ..arcTo(450, 350, 300, 100, 20)
+        ..stroke()
+        ..closePath();
+    
+    // Quadratic Curves
+    context..strokeStyle = 'blue'
+        ..beginPath()
+        ..moveTo(300, 0)
+        ..quadraticCurveTo(400, 25, 300, 50)
+        ..stroke()
+        ..closePath();
+    
+    // Bezier Curves
+    context..beginPath()
+        ..moveTo(300, 100)
+        ..bezierCurveTo(150, 225, 450, 275, 300, 400)
+        ..stroke()
+        ..closePath();
   }
 }
 
